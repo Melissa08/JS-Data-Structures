3689805a91259f02773bef518d5e2c0c66618b0a
melissa kaitesi <mulungimelissa@gmail.com>
2019-06-19 09:30:04 +0300

    edit

---
+++ arrays.js
@@ -0,0 +1,23 @@
+function isPrime (n)
+{
+    if (n<2)
+    return false;
+
+    /**
+     * An integer is prime if it is not divisible by any prime less than or equal to its square root
+     **/
+
+for (var j=2;j<= n; j++){ 
+      
+    if (j%n==0){
+        var arry[]=j;
+     for (var i =0; n <100)     
+        }
+    }
+
+    if (flag== true;){
+                print(i);
+    }
+}
+
+console.log(n<2==100)
